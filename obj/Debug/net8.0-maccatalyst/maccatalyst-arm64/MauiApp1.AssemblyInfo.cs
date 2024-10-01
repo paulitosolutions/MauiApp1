180cf1e7f624c491f77253e790a67dddbe72ea7d
Paulito <paulito@paulitos-air.speedport.ip>
2024-10-01 14:11:44 +0200

    fsfs

--- obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+20a1e0eba34f7e12bce8b61e90c3d2d795bd564b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3913e0cdbe575fcba41ef024d541dbebd3d2b8a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
